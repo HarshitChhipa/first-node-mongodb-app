2fbe0c7184efc66d4823e0c8e25af0a2afe9aa7b
Harshit Chhipa <harshit.kankerwal@milkbasket.com>
2021-01-09 13:06:56 +0530

    Added nodemon for smoooth dev process to compile code when i change anything in file

--- routes/tutorial.routes.js
+++ routes/tutorial.routes.js
@@ -1,7 +1,7 @@
 module.exports = app => {
     const tutorial = require("../controllers/tutorial.controller.js");
     var router = require("express").Router();
-
+    
     router.post("/",tutorial.create);
     // router.get("/", tutorial.findAll);
 
