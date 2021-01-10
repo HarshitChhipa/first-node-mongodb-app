083b1eb2b03caa602629399820eff28dc95c9456
Harshit Chhipa <harshitkankerwal@gmail.com>
2021-01-10 16:02:40 +0530

    Feature: Added: FindAll for api
    Feature: Added find by id

--- controllers/tutorial.controller.js
+++ controllers/tutorial.controller.js
@@ -9,21 +9,59 @@ exports.create = (req, res) => {
   }
 
   const tutorial = new Tutorial({
-      title: req.body.title,
-      description: req.body.description,
-      published: req.body.published ? req.body.published : false
+    title: req.body.title,
+    description: req.body.description,
+    published: req.body.published ? req.body.published : false,
   });
 
-  tutorial.save(tutorial).then(data => {
+  tutorial
+    .save(tutorial)
+    .then((data) => {
       res.send(data);
-  }).catch (err => {
+    })
+    .catch((err) => {
       res.status(500).send({
-        message: err.message || "Some error occurred while creating the tutorial."
+        message:
+          err.message || "Some error occurred while creating the tutorial.",
       });
-  });
+    });
 };
 
-exports.findAll = (req, res) => {};
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title
+    ? { title: { $regex: new RegExp(title), $options: "i" } }
+    : {};
+
+  Tutorial.find(condition)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials",
+      });
+    });
+};
+
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Tutorial.findById(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({ message: "Not found Tutorial with id " + id });
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Tutorial with id =" + id });
+    });
+};
 
 exports.update = (req, res) => {};
 
@@ -32,4 +70,3 @@ exports.delete = (req, res) => {};
 exports.deleteAll = (req, res) => {};
 
 exports.findAllPublished = (req, res) => {};
-
--- routes/tutorial.routes.js
+++ routes/tutorial.routes.js
@@ -1,9 +1,9 @@
-module.exports = app => {
-    const tutorial = require("../controllers/tutorial.controller.js");
-    var router = require("express").Router();
-    
-    router.post("/",tutorial.create);
-    // router.get("/", tutorial.findAll);
+module.exports = (app) => {
+  const tutorial = require("../controllers/tutorial.controller.js");
+  var router = require("express").Router();
 
-    app.use('/api/tutorials',router);
+  router.post("/", tutorial.create);
+  router.get("/", tutorial.findAll);
+
+  app.use("/api/tutorials", router);
 };
